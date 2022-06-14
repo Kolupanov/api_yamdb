5a97594d4ddf03fa0679bb68b7feb131938a9ee8
Андрей Миронов <amv_mail@mail.ru>
2022-06-14 22:48:29 +0300

    model Users

--- api_yamdb/User/models.py
+++ api_yamdb/User/models.py
@@ -4,9 +4,57 @@ from django.db import models
 
 
 class User(AbstractUser):
-    '''Добавление поля Биография.'''
-    bio = models.TextField(
-        'Биография',
-        help_text='Тут может быть биография пользователя',
-        blank=True,
+    '''Измененная модель User.'''
+    ADMIN = 'admin'
+    MODERATOR = 'moderator'
+    USER = 'user'
+    ROLES = [
+        (ADMIN, 'Administrator'),
+        (MODERATOR, 'Moderator'),
+        (USER, 'User'),
+    ]
+
+    email = models.EmailField(
+        verbose_name='Адрес электронной почты',
+        unique=True,
     )
+    username = models.CharField(
+        verbose_name='Имя пользователя',
+        max_length=200,
+        null=True,
+        unique=True
+    )
+    role = models.CharField(
+        verbose_name='Роль',
+        max_length=30,
+        choices=ROLES,
+        default=USER
+    )
+    bio = models.TextField(
+        verbose_name='Информация о себе',
+        null=True,
+        blank=True
+    )
+
+    @property
+    def is_moderator(self):
+        return self.role == self.MODERATOR
+
+    @property
+    def is_admin(self):
+        return self.role == self.ADMIN
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+        constraints = [
+            models.CheckConstraint(
+                check=~models.Q(username__iexact="me"),
+                name="username_is_not_me"
+            )
+        ]
