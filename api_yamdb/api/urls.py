0d06aaf8ed0714a814dc997bd58889f514965c34
Анатолий Колупанов <anatolykolupanov@gmail.com>
2022-06-18 18:30:00 +0300

    category, genre, title - done

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -17,9 +17,11 @@ router.register('genres', GenreViewSet, basename='genres')
 router.register(r'titles/(?P<title_id>\d+)/reviews',
                 ReviewViewSet,
                 basename='title-reviews')
-router.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
-                CommentViewSet,
-                basename='review-comments')
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='review-comments'
+)
 router.register(r'users', UserViewSet)
 
 
